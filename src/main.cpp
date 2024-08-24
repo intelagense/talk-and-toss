c028c3388fc8091d9f7e7c6780b9e78c361eb20f
Eric Wynn Romere <18331781+intelagense@users.noreply.github.com>
2024-08-24 05:43:46 -0500

    Defines alarms and connects to buttons for testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,105 @@
 #include <Arduino.h>
 
-const int button = 14;
-const int blinker = 0;
+//      Feather Huzzah with Modified Header pins
+//                   +-----+
+//      +------------| USB |------------+
+//      |            +-----+            |
+//      | [ ]RST                        |
+//      | [X]3.3V                       |
+//      | [ ]NC                         |
+//      | [X]GND                        |
+//      | [ ]ADC                 BAT[ ] |
+//      | [ ]GND                  EN[X] | PWR Switch
+//      | [X]GND                 USB[ ] |
+//      | [X]GND                  14[X] | Belly Button
+//      | [X]GND                  12[X] | Motor
+//      | [X]GND                  13[X] | Speaker
+//      | [ ]                     15[X] | LED
+//      | [ ]                      0[ ] |
+//      | [ ]                     16[ ] |
+//      | [ ]                      2[ ] |
+//      | [ ]                      5[ ] |
+//      | [ ]                      4[ ] |
+//      |                               |
+//      +-------------------------------+
+
+
+const int bellyButton = 14;
+const int blinker = 15;
+const int motor = 12;
+const int speaker = 13;
+
+void alarm();
+void silentAlarm();
+void startAlarm();
+void wakeUpChirp();
 
 void setup() {  
-  pinMode(button, INPUT_PULLUP);
+  pinMode(bellyButton, INPUT_PULLUP);
   pinMode(blinker, OUTPUT);
-  digitalWrite(blinker, HIGH);
+  pinMode(motor, OUTPUT);
+  pinMode(speaker, OUTPUT);
+
+  wakeUpChirp();
+  delay(1000);
 }
 
 void loop() {
-  int buttonState = digitalRead(button);
+  int buttonState = digitalRead(bellyButton);
 
   if (buttonState == LOW) {
-    digitalWrite(blinker, LOW);
-  } else {
     digitalWrite(blinker, HIGH);
+
+    for (int i = 0; i < 5; i++) {
+      silentAlarm();
+      delay(200);
+    }
+  } else {
+    digitalWrite(blinker, LOW);
+    digitalWrite(motor, LOW);
+    noTone(speaker);
   }
 
   // debounce
   delay(50);
+}
+
+void wakeUpChirp() {
+  for (int i = 0; i < 5; i++) {
+    int randomFrequency = random(800, 2000);
+    int randomDuration = random(100, 400);
+    tone(speaker, randomFrequency, randomDuration);
+    delay(random(50, 200));
+  }
+  delay(200);
+  tone(speaker, 1500, 100);
+  delay(200);
+  tone(speaker, 1700, 100);
+  delay(200);
+  tone(speaker, 1500, 100);
+  delay(200);
+  tone(speaker, 1900, 300);
+  delay(400);
+}
+
+void alarm() {
+  tone(speaker, 1000, 200);
+  digitalWrite(motor, HIGH);
+  delay(300);
+  digitalWrite(motor, LOW);
+  tone(speaker, 1500, 400);
+  delay(600);
+}
+
+void silentAlarm() {
+  for (int i = 0; i < 3; i++) { 
+    digitalWrite(motor, HIGH);
+    delay(50);
+    digitalWrite(motor, LOW);
+    delay(50);
+    digitalWrite(motor, HIGH);
+    delay(50);
+    digitalWrite(motor, LOW);
+    delay(50);
+  }
 }
